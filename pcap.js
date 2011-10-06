f02762d01384eacc8ba1803a67f2db92090fa61a
Peter Sanford <psanford@nearbuysystems.com>
2011-10-06 11:01:36 -0700

    Stop readWatcher on session.close.
    Without this, the node process would segfault in
    the readWatcher trying to read from a closed fd (with an
    offline session).

--- pcap.js
+++ pcap.js
@@ -80,7 +80,8 @@ Pcap.prototype.open = function (live, device, filter, buffer_size, pcap_output_f
 
 Pcap.prototype.close = function () {
     this.opened = false;
-    this.session.close();
+    this.readWatcher.stop();
+    binding.close();
     // TODO - remove listeners so program will exit I guess?
 };
 
